4e0134177a1af3a95438e5571e92c4c9f5a7cdf1
vafa <wfa17269@gmail.com>
2026-07-28 07:34:41 -0400

    refactor(attendance): simplify approved timeoff signal handler

--- apps/api/attendance/signals.py
+++ apps/api/attendance/signals.py
@@ -25,21 +25,3 @@ def handle_approved_timeoff(sender, instance, created, **kwargs):
                 
             instance._signal_processed = True
             attendance.save(update_fields=['overtime_minutes'])
-
-
-@receiver(post_save, sender=Attendance)
-def calculate_auto_overtime(sender, instance, created, **kwargs):
-    if not hasattr(instance, '_overtime_calculated'):
-        if instance.check_out and instance.check_in:
-            setting = AttendanceSetting.objects.filter(organization=instance.organization).first()
-            if setting:
-                duration = (instance.check_out - instance.check_in).total_seconds() / 3600.0
-                expected = float(setting.expected_daily_hours)
-                
-                # Check if it exceeds expected hours
-                if duration > expected:
-                    auto_overtime = int((duration - expected) * 60)
-                    if instance.overtime_minutes < auto_overtime:
-                        instance.overtime_minutes = auto_overtime
-                        instance._overtime_calculated = True
-                        instance.save(update_fields=['overtime_minutes'])
