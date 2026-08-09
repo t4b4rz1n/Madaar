e54ae13f10fcede84a361fdb9ce47d2405f5adac
vafa <wfa17269@gmail.com>
2026-08-09 14:53:51 -0400

    chore: remove all GitHub workflow automation code as requested

--- apps/api/automations/urls.py
+++ apps/api/automations/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import TelegramWebhookView, GithubWebhookView, GenerateTelegramMagicLinkView
+from .views import TelegramWebhookView, GenerateTelegramMagicLinkView
 
 urlpatterns = [
     path('telegram/webhook/', TelegramWebhookView.as_view(), name='telegram_webhook'),
     path('telegram/magic-link/', GenerateTelegramMagicLinkView.as_view(), name='telegram_magic_link'),
-    path('github/webhook/', GithubWebhookView.as_view(), name='github_webhook'),
 ]
