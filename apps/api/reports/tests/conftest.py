67d39014ea8e3602908a8a7b7fd83f2387ea15ac
Alireza1429 <alirezamokhtary1429@gmail.com>
2026-08-11 22:19:47 +0330

    fix(reports/tests): correct DJANGO_SETTINGS_MODULE path in conftest.py

--- apps/api/reports/tests/conftest.py
+++ apps/api/reports/tests/conftest.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apps.api.config.test_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.test_settings')
 import django
 django.setup()
 import datetime
