a4d9ee0f9b5d4256059595a233405f8ef5723b9f
Alireza1429 <alirezamokhtary1429@gmail.com>
2026-08-11 21:30:34 +0330

    test: add pytest config and in-memory cache test settings

--- apps/api/reports/tests/conftest.py
+++ apps/api/reports/tests/conftest.py
@@ -1,3 +1,7 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apps.api.config.test_settings')
+import django
+django.setup()
 import datetime
 import pytest
 from django.utils import timezone
