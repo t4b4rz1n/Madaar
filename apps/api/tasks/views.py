6e44e90f514e0191454510535d3de257fa2ec45c
vafa <wfa17269@gmail.com>
2026-08-16 16:11:10 -0400

    git commit -m "refactor: remove unused variable other_org_ids from tasks views"

--- apps/api/tasks/views.py
+++ apps/api/tasks/views.py
@@ -562,7 +562,7 @@ class AsyncStandupViewSet(viewsets.ModelViewSet):
         else:
             memberships = user.org_memberships.all()
             admin_org_ids = [m.organization_id for m in memberships if m.role.lower() in ["owner", "admin"]]
-            other_org_ids = [m.organization_id for m in memberships if m.role.lower() not in ["owner", "admin"]]
+            [m.organization_id for m in memberships if m.role.lower() not in ["owner", "admin"]]
 
             qs = AsyncStandup.objects.select_related("user").filter(is_deleted=False)
 
