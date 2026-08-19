82e11867305fbffae3fd1d46bbb64e17cb16bf90
MrMohammadHosein <mohammadhamze1387@gmail.com>
2026-08-20 00:52:33 +0330

    feat(accounts): integrate organization endpoints and add user profile services

--- apps/api/accounts/serializers.py
+++ apps/api/accounts/serializers.py
@@ -2,7 +2,96 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
+from .models import UserProfile
+
 User = get_user_model()
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = (
+            "id",
+            "job_title",
+            "department",
+            "employee_id",
+            "employment_type",
+            "hire_date",
+            "bio",
+            "skills",
+            "experience_history",
+            "github_url",
+            "linkedin_url",
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    avatar_url = serializers.SerializerMethodField()
+    profile = UserProfileSerializer(required=False)
+    password = serializers.CharField(
+        write_only=True, required=False, validators=[validate_password]
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "avatar",
+            "avatar_url",
+            "is_active",
+            "is_staff",
+            "password",
+            "profile",
+            "created_at",
+            "updated_at",
+        )
+        read_only_fields = ("id", "created_at", "updated_at")
+
+    def get_avatar_url(self, obj):
+        if not obj.avatar:
+            return None
+        request = self.context.get("request")
+        image_url = obj.avatar.url
+        return request.build_absolute_uri(image_url) if request else image_url
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop("profile", None)
+        password = validated_data.pop("password", None)
+        user = User.objects.create_user(**validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        if profile_data:
+            UserProfile.objects.create(user=user, **profile_data)
+        else:
+            UserProfile.objects.create(user=user)
+
+        return user
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop("profile", None)
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+        instance.save()
+
+        if profile_data:
+            profile, _created = UserProfile.objects.get_or_create(user=instance)
+            for attr, value in profile_data.items():
+                setattr(profile, attr, value)
+            profile.save()
+
+        return instance
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
--- apps/api/organizations/apps.py
+++ apps/api/organizations/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class OrganizationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "organizations"
+
+    def ready(self):
+        from access_control.permissions import set_resolver
+
+        from .permissions.bootstrap import get_organization_resolver
+
+        set_resolver(get_organization_resolver())
